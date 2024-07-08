20d863f65b5de084a7cf1ff2b7b9f98d71be54a7
chenxiaolong <chenxiaolong0001@foxmail.com>
2024-07-08 19:53:44 +0800

    download error with try

--- lib/pages/musiclist/sourcemanager.dart
+++ lib/pages/musiclist/sourcemanager.dart
@@ -37,36 +37,41 @@ class SourceManager {
   }
 
   Future<List<Musicinfo>> _loadMusicInfosFromRepo(Repoinfo repo) async {
-    final shares = await SambaBrowser.getShareList(
-      'smb://${repo.ip}/${repo.path}',
-      '',
-      repo.username,
-      repo.password,
-    );
-
-    final supportedExtensions = ['mp3', 'flac', 'wav'];
     List<Musicinfo> musicInfos = [];
 
-    List<String> urls = shares.cast<String>();
-    for (var share in urls) {
-      // Get the file extension
-      String extension = share.split('.').last;
+    try {
+      final shares = await SambaBrowser.getShareList(
+        'smb://${repo.ip}/${repo.path}',
+        '',
+        repo.username,
+        repo.password,
+      );
 
-      // Check if the file is a supported music file
-      if (supportedExtensions.contains(extension.toLowerCase())) {
-        // Assuming the share name is the file name
-        String name = share.split('/').last.split('.').first;
-        // You can set artist and album as empty strings for now, since this information might not be available
-        String artist = "";
-        String album = "";
+      final supportedExtensions = ['mp3', 'flac', 'wav'];
 
-        // Create a Musicinfo instance and set the URL
-        Musicinfo musicInfo = Musicinfo(
-            name: name, artist: artist, album: album, url: share, repo: repo);
+      List<String> urls = shares.cast<String>();
+      for (var share in urls) {
+        // Get the file extension
+        String extension = share.split('.').last;
 
-        // Add the instance to the list
-        musicInfos.add(musicInfo);
+        // Check if the file is a supported music file
+        if (supportedExtensions.contains(extension.toLowerCase())) {
+          // Assuming the share name is the file name
+          String name = share.split('/').last.split('.').first;
+          // You can set artist and album as empty strings for now, since this information might not be available
+          String artist = "";
+          String album = "";
+
+          // Create a Musicinfo instance and set the URL
+          Musicinfo musicInfo = Musicinfo(
+              name: name, artist: artist, album: album, url: share, repo: repo);
+
+          // Add the instance to the list
+          musicInfos.add(musicInfo);
+        }
       }
+    } catch (e) {
+      print('Error list: $e');
     }
     return musicInfos;
   }
